8714cae17bd582f83f130205ef74653aeb2f9e7e
Derek <dxiang1391458323@foxmail.com>
2019-05-11 17:50:36 +0800

    update cookies

--- middleware/m2.js
+++ middleware/m2.js
@@ -1,12 +1,12 @@
-function m1(ctx) {
-  global.console.log("m1 middleware:");
+function m2(ctx) {
+  global.console.log("m2 middleware:");
 }
 
 module.exports = function() {
   return async function(ctx, next) {
-    global.console.log('m1 start')
-    m1(ctx);
+    global.console.log('m2 start')
+    m2(ctx);
     await next(); // 下一个中间件处理
-    global.console.log('m1 end')
+    global.console.log('m2 end')
   };
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("koa-router")();
 
 router.get("/", async (ctx, next) => {
+  ctx.cookies.set("pvid", Math.random());
   global.console.log("test console log");
   await ctx.render("index", {
     title: "Hello Koa 2!"
@@ -13,7 +14,8 @@ router.get("/string", async (ctx, next) => {
 
 router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: "koa2 json"
+    title: "koa2 json",
+    cookies: ctx.cookies.get('pvid')
   };
 });
 
