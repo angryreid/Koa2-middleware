a8c4f03622a485392b05d45f794cdb414299f54e
Derek <dxiang1391458323@foxmail.com>
2019-05-12 10:09:03 +0800

    update cookies

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const json = require("koa-json");
 const onerror = require("koa-onerror");
 const bodyparser = require("koa-bodyparser");
 const logger = require("koa-logger");
+const mongoose = require("mongoose");
+const dbConfig = require("./dbs/config");
+const Redis = require("koa-redis");
+const session = require("koa-generic-session");
 const pv = require("./middleware/koa-pv");
-const m1 = require("./middleware/m1");
-const m2 = require("./middleware/m2");
-const m3 = require("./middleware/m3");
 
 const index = require("./routes/index");
 const users = require("./routes/users");
@@ -16,16 +17,23 @@ const users = require("./routes/users");
 // error handler
 onerror(app);
 
+// session
+app.keys = ["heys", "keyssss"];
+app.use(
+  session({
+    key: "com",
+    prefix: "cn",
+    store: new Redis()
+  })
+);
+
 // middlewares
+app.use(pv())
 app.use(
   bodyparser({
     enableTypes: ["json", "form", "text"]
   })
 );
-app.use(pv());
-app.use(m1());
-app.use(m2());
-app.use(m3());
 app.use(json());
 app.use(logger());
 app.use(require("koa-static")(__dirname + "/public"));
@@ -47,6 +55,18 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods());
 app.use(users.routes(), users.allowedMethods());
+
+// mongoose
+mongoose
+  .connect(dbConfig.dbs, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    global.console.log("success connected to mongodb");
+  })
+  .catch(err => {
+    global.console.log(err);
+  });
 
 // error-handling
 app.on("error", (err, ctx) => {
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,90 @@
-const router = require('koa-router')()
+const router = require("koa-router")();
+const Person = require("../dbs/models/person");
+const Redis = require("koa-redis");
 
-router.prefix('/users')
+const Store = new Redis().client;
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.prefix("/users");
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+router.get("/", function(ctx, next) {
+  ctx.body = "this is a users response!";
+});
 
-module.exports = router
+router.get("/bar", function(ctx, next) {
+  ctx.body = "this is a users/bar response";
+});
+
+// create
+router.post("/addPerson", async function(ctx) {
+  const person = new Person({
+    name: ctx.request.body.name,
+    age: ctx.request.body.age
+  });
+  let code = 0;
+  try {
+    await person.save();
+  } catch (error) {
+    code = -1;
+  }
+  ctx.body = {
+    code
+  };
+});
+
+// find
+router.get("/getPerson", async function(ctx) {
+  let code = 0;
+  try {
+    const result = await Person.findOne({ name: ctx.request.body.name });
+    const results = await Person.find({ name: ctx.request.body.name });
+  } catch (error) {
+    code = -1;
+  }
+  ctx.body = {
+    code,
+    result,
+    results
+  };
+});
+
+// update
+router.post("updatePerson", async function(ctx) {
+  let code = 0;
+  try {
+    const result = await Person.where({
+      name: ctx.request.body.name
+    }).update({
+      age: ctx.request.body.age
+    });
+  } catch (error) {
+    code = -1;
+  }
+  ctx.body = {
+    code
+  };
+});
+
+// remove
+router.post("removePerson", async function(ctx) {
+  let code = 0;
+  try {
+    const result = await Person.where({
+      name: ctx.request.body.name
+    }).remove();
+  } catch (error) {
+    code = -1;
+  }
+  ctx.body = {
+    code
+  };
+});
+
+// redis
+router.get("/fix", async function(ctx) {
+  const st = await Store.hset("fix", "name", Math.random());
+  ctx.body = {
+    code: 0
+  };
+});
+
+module.exports = router;
